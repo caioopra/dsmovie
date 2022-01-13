54ba3b11bcfdc0469f4d0d052731b59caf9a8f79
Caio Pra <caioprsilva@gmail.com>
2022-01-13 00:08:16 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
-import Navbar from "./components/Navbar";
+import {  // componentes de react-router-dom
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';  // componentes React criados para usar em cada rota
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>  // inicia configuração das rotas
   );
 }
 
